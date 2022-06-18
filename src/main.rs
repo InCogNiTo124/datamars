6d584cbcf245f24d610c648ca8bb50e933d514a6
InCogNiTo124 <msmetko@msmetko.xyz>
2022-06-18 23:30:43 +0200

    Working mean

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use clap::{Arg, Command};
 use std::io::BufRead;
 
+#[derive(Default)]
+struct Mean {
+    n: i64,
+    result: f64,
+}
+
+impl Mean {
+    fn apply(&mut self, x: f64) {
+        self.n += 1;
+        self.result += (x - self.result) / (self.n as f64);
+    }
+
+    fn new() -> Mean {
+        Mean {n: 0, result: 0.0}
+    }
+}
+
 fn main() {
     let parser = Command::new("ms")
         .author("Marijan Smetko, msmetko@msmetko.xyz")
-        .version("0.0.1")
+        .version("0.0.2")
         .arg(
             Arg::new("delimiter")
                 .short('d')
@@ -14,16 +31,20 @@ fn main() {
         .arg(Arg::new("commands").multiple_values(true));
     let matches = parser.get_matches();
     let delimiter = matches.value_of("delimiter").unwrap();
-    println!("{:?}", delimiter);
-    let vals: Vec<&str> = matches.values_of("commands").unwrap().collect();
+    let vals: Vec<&str> = matches
+        .values_of("commands")
+        .expect("No commands provided")
+        .collect();
     assert!(vals.len() == 2);
     assert!(vals[0].eq("mean"));
-    assert!(vals[1].eq("1"));
-    println!("{:?}", vals);
-    let locked_stdin = std::io::stdin().lock();
-    for line in locked_stdin.lines() {
+    let index = vals[1].parse::<usize>().unwrap() - 1;
+    let mut mean = Mean::new();
+    let locked_stdin = std::io::stdin();
+    for line in locked_stdin.lock().lines() {
         let _line = line.unwrap();
         let parts: Vec<&str> = _line.split(delimiter).collect();
-        println!("{:?}", parts);
+        let number = parts[index].parse::<f64>().unwrap();
+        mean.apply(number);
     }
+    println!("{}", mean.result);
 }
