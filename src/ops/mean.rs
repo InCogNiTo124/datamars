ae8c541aa1f0584ef4cc2e7fe0a4908348e068ad
InCogNiTo124 <msmetko@msmetko.xyz>
2022-07-28 00:27:19 +0200

    Add `ops::Mean` tests

--- src/ops/mean.rs
+++ src/ops/mean.rs
@@ -21,3 +21,29 @@ impl Mean {
         Self { n: 0, result: 0.0 }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{Mean, Operator};
+
+    #[test]
+    fn test1() {
+        let mut obj = Mean::new();
+        let test_cases = vec![
+            (1, 1.0),
+            (2, 1.5),
+            (3, 2.0),
+            (4, 2.5),
+            (5, 3.0),
+            (6, 3.5),
+            (7, 4.0),
+            (8, 4.5),
+            (9, 5.0),
+            (100, 14.5),
+        ];
+        for (x, y) in test_cases {
+            obj.apply(x as f64);
+            assert!((obj.result() - y).abs() < 1e-7);
+        }
+    }
+}
