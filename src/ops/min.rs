2a225c15037fdd5f953975f9df263b7d3cd31526
InCogNiTo124 <msmetko@msmetko.xyz>
2022-08-14 21:29:12 +0200

    Add `Min` tests

--- src/ops/min.rs
+++ src/ops/min.rs
@@ -18,6 +18,35 @@ impl Operator for Min {
 
 impl Min {
     pub const fn new() -> Self {
-        Self { result: f64::INFINITY }
+        Self {
+            result: f64::INFINITY,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Min, Operator};
+
+    #[test]
+    fn test1() {
+        let mut obj = Min::new();
+        let test_cases = vec![
+            (1, 1.0),
+            (2, 1.0),
+            (3, 1.0),
+            (4, 1.0),
+            (5, 1.0),
+            (6, 1.0),
+            (7, 1.0),
+            (8, 1.0),
+            (9, 1.0),
+            (100, 1.0),
+        ];
+        for (x, y) in test_cases {
+            obj.apply(x as f64);
+            let error = (obj.result() - y).abs();
+            assert!(error < 1e-15);
+        }
     }
 }
