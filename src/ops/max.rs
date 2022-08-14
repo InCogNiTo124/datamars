c9b06084c35cc1435456e46f6c539d9a4f4aafde
InCogNiTo124 <msmetko@msmetko.xyz>
2022-08-14 21:29:02 +0200

    Add `Max` tests

--- src/ops/max.rs
+++ src/ops/max.rs
@@ -18,6 +18,34 @@ impl Operator for Max {
 
 impl Max {
     pub const fn new() -> Self {
-        Self { result: f64::NEG_INFINITY }
+        Self {
+            result: f64::NEG_INFINITY,
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::{Max, Operator};
+
+    #[test]
+    fn test1() {
+        let mut obj = Max::new();
+        let test_cases = vec![
+            (1, 1.0),
+            (2, 2.0),
+            (3, 3.0),
+            (4, 4.0),
+            (5, 5.0),
+            (6, 6.0),
+            (7, 7.0),
+            (8, 8.0),
+            (9, 9.0),
+            (100, 100.0),
+        ];
+        for (x, y) in test_cases {
+            obj.apply(x as f64);
+            let error = (obj.result() - y).abs();
+            assert!(error < 1e-15);
+        }
     }
 }
